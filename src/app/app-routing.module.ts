0d514e2ae4e8b2cb631907ac48144713e424f14d
Aicha Benjdir <aicha.benjdir@isimg.tn>
2024-11-26 20:29:33 +0100

    Intégration d'un template HTML et CSS dans le cadre du module Angular de la formation 10000 Codeurs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VoyagesComponent } from './voyages/voyages.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'voyages', pathMatch: 'full' }, // Redirection vers la page par défaut
+  { path: 'voyages', component: VoyagesComponent }, // Page Voyages
+  { path: 'reservation', component: ReservationComponent }, // Page Réservation
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoyagesComponent } from './voyages/voyages.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoyagesComponent,
+    ReservationComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
